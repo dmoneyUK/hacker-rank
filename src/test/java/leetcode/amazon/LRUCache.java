274b5d25bf5d3e4bcaca131721366b6743ea8e83
Jinge Dai <jinge.dai@gamesys.co.uk>
2019-04-08 23:04:20 +0100

    Completed LRU test

--- src/test/java/leetcode/amazon/LRUCache.java
+++ src/test/java/leetcode/amazon/LRUCache.java
@@ -1,11 +1,53 @@
 package leetcode.amazon;
 
-import lombok.Setter;
+import java.util.HashMap;
+import java.util.Map;
 
-public class LRUCache {
+public class LRUCache implements MyCache {
     
     private LinkedNode dummyHead;
     private LinkedNode dummyTail;
+    private Map<Integer, LinkedNode> cache = new HashMap<>();
+    private int capacity;
+    private int count;
+    
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        dummyHead = new LinkedNode();
+        dummyTail = new LinkedNode();
+        dummyHead.next = dummyTail;
+        dummyTail.pre = dummyHead;
+    }
+    
+    public int get(int key) {
+        LinkedNode node = cache.get(key);
+        if (node == null) {
+            return -1;
+        }
+        
+        removeNode(node);
+        addNode(node);
+        
+        return node.value;
+    }
+    
+    public void put(int key, int value) {
+        
+        LinkedNode existed = cache.get(key);
+        if (existed != null) {
+            removeNode(existed);
+        } else {
+            count++;
+            if (count > capacity) {
+                cache.remove(dummyTail.pre.key);
+                removeNode(dummyTail.pre);
+            }
+        }
+        LinkedNode node = new LinkedNode(key, value);
+        cache.put(key, node);
+        addNode(node);
+        
+    }
     
     public void addNode(LinkedNode node) {
         LinkedNode oldHead = dummyHead.next;
@@ -22,17 +64,77 @@ public class LRUCache {
         next.pre = pre;
     }
     
-    @Setter
     class LinkedNode {
         private int key;
         private int value;
         private LinkedNode pre;
         private LinkedNode next;
         
+        public LinkedNode() {
+        }
+        
         public LinkedNode(int key, int value) {
             this.key = key;
             this.value = value;
         }
+    }
+    
+    public static void main(String args[]) {
+        MyCache cache = new MonitoredCache(new LRUCache(2));
+        cache.get(2);
+        cache.put(2, 6);
+        cache.get(1);
+        cache.put(1, 5);
+        cache.put(1, 2);
+        cache.get(1);
+        cache.get(2);
+    }
+}
+
+interface MyCache {
+    
+    int get(int key);
+    
+    void put(int key, int value);
+}
+
+abstract class MyCacheDecorator implements MyCache {
+    
+    private MyCache cacheToBeDecorated;
+    
+    public MyCacheDecorator(MyCache cache) {
+        cacheToBeDecorated = cache;
+    }
+    
+    @Override
+    public int get(int key) {
+        return cacheToBeDecorated.get(key);
+    }
+    
+    @Override
+    public void put(int key, int value) {
+        cacheToBeDecorated.put(key, value);
+    }
+}
+
+class MonitoredCache extends MyCacheDecorator {
+    
+    public MonitoredCache(MyCache cache) {
+        super(cache);
+    }
+    
+    @Override
+    public int get(int key) {
+        int ans = super.get(key);
+        System.out.println(ans);
+        return ans;
+        
+    }
+    
+    @Override
+    public void put(int key, int value) {
+        super.put(key, value);
+        System.out.println("null");
     }
     
 }
